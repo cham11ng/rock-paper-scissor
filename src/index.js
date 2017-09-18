399483df4c2abbce911595907dac4ce241ace18f
Sagar Chamling <sgr.raee@gmail.com>
2017-09-18 16:43:12 +0545

    Create bundle

---
+++ src/index.js
@@ -0,0 +1,11 @@
+window._ = require('lodash');
+
+function component() {
+	var element = document.createElement('div');
+
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+	return element;
+}
+
+document.body.appendChild(component());
