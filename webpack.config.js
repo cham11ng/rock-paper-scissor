22f4be8be2b343dded7874df1fc02c2a0896a700
Sagar Chamling <sgr.raee@gmail.com>
2017-09-21 00:27:03 +0545

    User webpack-dev-middleware for custom setups with express server.

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
 	},
 	output: {
 		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/'
 	},
 	plugins: [
 		new CLeanWebpackPlugin(['dist']),
