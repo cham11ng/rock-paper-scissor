57a1d74942138f09b5f27dc50e8364c8d42136b4
Sagar Chamling <sgr.raee@gmail.com>
2017-09-21 12:14:31 +0545

    Re-render component in Hot module replacement.

--- src/js/index.js
+++ src/js/index.js
@@ -23,11 +23,14 @@ function component() {
 	return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // store the element to re-render on print.js changes
+document.body.appendChild(element);
 
 if (module.hot) {
 	module.hot.accept('./print.js', function() {
-		console.log('Accepting the updated printMe module!');
-		PrintMe();
+		console.log('Accepting tohe updated printMe module!');
+		document.body.removeChild(element);
+		element = component(); // re-render the "component" to update the click handler
+		document.body.appendChild(element);
 	})
 }
